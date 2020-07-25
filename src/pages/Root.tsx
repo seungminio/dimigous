1fa99f589f568850d664e8775c5d8e46162a7b5b
Lee Seungmin <lee@seungmin.io>
2020-07-25 22:21:20 +0900

    fix: main page style

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Input from '../components/Input';
+import InputWithLabel from '../components/InputWithLabel';
 
 const Root: React.FC = () => {
   return (
     <Container>
       <Box>
         <LogoWrapper>
-          <Logo src={require('../assets/dimigo_logo.png')} />
+          <Logo src="https://www.dimigo.hs.kr/layouts/minimal_dimigo/static/dimigo_logo_white.png" />
         </LogoWrapper>
         <Info>
           디미고 계정으로 <b>디미고인</b> 서비스에 로그인합니다.
         </Info>
         <InputWrap>
-          <Input id="id" type="text" placeholder="아이디" />
-          <Input id="password" type="password" placeholder="비밀번호" />
+          <InputWithLabel label="아이디" id="id" type="text" />
+          <InputWithLabel label="비밀번호" id="password" type="password" />
         </InputWrap>
         <Button>로그인</Button>
       </Box>
@@ -26,8 +27,8 @@ const Container = styled.div`
   display: flex;
   width: 100%;
   height: 100vh;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-end;
+  /* align-items: center; */
   background-image: url('https://www.dimigo.hs.kr/layouts/minimal_dimigo/images/background.jpg');
   background-color: black;
   background-size: cover;
@@ -36,14 +37,14 @@ const Container = styled.div`
 const Box = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
   width: 100%;
-  max-width: 610px;
-  background-color: white;
+  max-width: 580px;
+  background-color: rgba(0, 0, 0, 0.85);
+  color: white;
   padding: 80px;
-  border-radius: 50px;
-  box-shadow: 20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff;
+  /* border-radius: 50px; */
+  /* box-shadow: 20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff; */
 `;
 
 const LogoWrapper = styled.div`
@@ -58,15 +59,15 @@ const Logo = styled.img`
 const Info = styled.span`
   font-size: 20px;
   padding-bottom: 15px;
-  border-bottom: 1px solid #999;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.7);
   margin-bottom: 40px;
 `;
 
 const InputWrap = styled.div`
   width: 100%;
-  margin-bottom: 40px;
+  margin-bottom: 55px;
   & > * {
-    margin-bottom: 25px;
+    margin-bottom: 20px;
     &: last-child {
       margin-bottom: 0;
     }
@@ -74,13 +75,15 @@ const InputWrap = styled.div`
 `;
 
 const Button = styled.button`
-  height: 55px;
+  height: 44px;
+  border-radius: 3px;
   cursor: pointer;
-  border-radius: calc(55px / 2);
+  /* border-radius: calc(55px / 2); */
   width: 100%;
   font-size: 20px;
   border: 0;
-  padding: 17px;
+  padding: 2px 16px;
+  font-size: 16px;
   background-color: #da367f;
   color: white;
   outline: none;
