71ab87b5b7381252450b532e2410cbea7d55a75d
Lee Seungmin <lee@seungmin.io>
2020-07-26 12:02:51 +0900

    fix: Routing setting

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import { Root } from "../pages";
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { Root } from '../pages';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Root} />
+      <Route exact path="/global" component={Root} />
+      <Route exact path="/oauth2.0/authorize" component={Root} />
     </Switch>
   </BrowserRouter>
 );
